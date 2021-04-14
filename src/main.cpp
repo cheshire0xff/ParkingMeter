9e31d37f6bbeab8b60a299fa9aa7fa559f767b5a
cheshire <fantasyofwhiteness@gmail.com>
2021-04-14 21:40:44 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : SystemCTest.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <systemc.h>
+#include "config.h"
+
+using namespace std;
+
+int sc_main(int, char**) {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
